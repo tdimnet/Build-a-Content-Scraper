6de1b34cef24c1db886273bed23119d45c52d5ed
tdimnet <thomas.dimnet@gmail.com>
2017-05-07 19:28:19 +0200

    The scraper gets me the t-shirts link for the home page. I now need to access the shirts page

--- scraper.js
+++ scraper.js
@@ -1,6 +1,9 @@
 // Create a folder called data if it does not already exist inside the folder
 const fs = require('fs');
 
+// The web scraper
+const scrapeIt = require('scrape-it');
+
 
 // Check if the folder exists or not. If not, create a new one
 const check = fs.stat('./data/', (err, stats) => {
@@ -14,5 +17,35 @@ const check = fs.stat('./data/', (err, stats) => {
 
 
 // Choose and use two third-party npm packages.
-    // One package should be used to scrape content from the site.
+    // One package should be used to scrape content from the site
+        // For now the choosen scraper is scrape-it
     // The other package should create the CSV file.
+
+
+// Program your scraper so that it visits the website and uses the shirts.php as single entry point
+scrapeIt('http://www.shirts4mike.com/', {
+    // listItem: 'h1',
+    // url: {
+    //     selector: '.nav .shirts a',
+    //     attr: 'href'
+    // }
+
+    // This function gets me the t-shirts from the home page, I now need to access the shirts page
+    shirts: {
+        listItem: '.products li',
+        name: 'shirts',
+        data: {
+            title: 'a',
+            url: {
+                selector: 'a',
+                attr: 'href'
+            }
+        }
+
+    }
+
+}).then(function(value) {
+    console.log(value);
+}, function(raison) {
+    console.log(raison);
+});
