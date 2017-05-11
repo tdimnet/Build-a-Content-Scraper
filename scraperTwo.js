aa2ef847fb23ca37b8c7b227c6b458d437954a71
tdimnet <thomas.dimnet@gmail.com>
2017-05-11 18:51:55 +0200

    The script creates now a csv file which contains all the urls.

--- scraperTwo.js
+++ scraperTwo.js
@@ -2,23 +2,24 @@
 const fs = require('fs');
 // The web scraper
 const scrapeIt = require('scrape-it');
+// The JSON to CSV
+var json2csv = require('json2csv');
+
 
 (function() {
+    // First step: go the shirts4mike.com website
     scrapeIt("http://www.shirts4mike.com/", {
         url: {
             selector: '.nav .shirts a',
             attr: 'href'
         }
     }).then((value) => {
-        let shirtUrl = value.url;
-        return shirtUrl;
-    }).then( function(value) {
-        scrapeIt('http://www.shirts4mike.com/'+value, {
+        // Then take all the urls needed
+        scrapeIt('http://www.shirts4mike.com/'+value.url, {
             shirts: {
             listItem: '.products li',
             name: 'shirts',
             data: {
-                // title: 'a',
                 url: {
                     selector: 'a',
                     attr: 'href'
@@ -26,7 +27,17 @@ const scrapeIt = require('scrape-it');
                 }
             }
         }).then((value) => {
-            console.log(value)
+            // For now, we are displaying them in an array of url
+            // console.log(value.shirts);
+            let fields = ['url'];
+            let urlPages = value.shirts;
+            let csv = json2csv({ data: urlPages, fields: fields });
+
+            fs.writeFile('file.csv', csv, function(err) {
+              if (err) throw err;
+              console.log('file saved');
+            });
+
         })
     });
 })();
